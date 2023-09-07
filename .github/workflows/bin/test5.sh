a9f9ec79823295e5cf3fb131fb7ba3645b585a8a
hayato0311 <hayato8088@gmail.com>
2023-09-07 11:43:49 +0900

    aaa

--- .github/workflows/bin/test5.sh
+++ .github/workflows/bin/test5.sh
@@ -5,7 +5,7 @@ echo "Apply Schemafile" >> $tmpfile
 echo "[ERROR] Mysql2::Error: Invalid use of NULL value" >> $tmpfile
 echo "Finished ridgepole apply." >> $tmpfile
 
-RIDGEPOLE_APPLY_LOG=$(cat $tmpfile | sed 's/%/%25/g' | sed 's/\r/%0D/g' | sed 's/\n/%0A/g' | sed 's/`/%60/g' | sed 's/::/%3A%3A/g' | sed 's/"/%22/g')
+RIDGEPOLE_APPLY_LOG=$(cat $tmpfile | sed 's/%/%25/g' | sed 's/\r/%0D/g' | sed 's/\n/%0A/g' | sed 's/`/%60/g' | sed 's/::/%3A%3A/g' | sed 's/" /%22/g' | sed 's/ /%20/g')
 
 echo "$RIDGEPOLE_APPLY_LOG"
 
