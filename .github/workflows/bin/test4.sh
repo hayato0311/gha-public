37ad82bf093cbc4de5fea9422429b8537a3dec39
hayato0311 <hayato8088@gmail.com>
2023-09-07 11:26:36 +0900

    add

--- .github/workflows/bin/test4.sh
+++ .github/workflows/bin/test4.sh
@@ -5,6 +5,6 @@ echo "Apply Schemafile" >> $tmpfile
 echo "[ERROR] Mysql2::Error: Invalid use of NULL value" >> $tmpfile
 echo "Finished ridgepole apply." >> $tmpfile
 
-echo "RIDGEPOLE_APPLY_LOG=$(cat $tmpfile)" >> $GITHUB_OUTPUT
+# echo "RIDGEPOLE_APPLY_LOG=$(cat $tmpfile)" >> $GITHUB_OUTPUT
 
 rm $tmpfile
