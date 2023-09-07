0d3155a6e730fc2828f97eb5f0cf26937f5eb224
hayato0311 <hayato8088@gmail.com>
2023-09-07 11:41:41 +0900

    aaa

--- .github/workflows/bin/test5.sh
+++ .github/workflows/bin/test5.sh
@@ -7,7 +7,7 @@ echo "Finished ridgepole apply." >> $tmpfile
 
 RIDGEPOLE_APPLY_LOG=$(cat $tmpfile | sed 's/%/%25/g' | sed 's/\r/%0D/g' | sed 's/\n/%0A/g' | sed 's/`/%60/g' | sed 's/::/%3A%3A/g' | sed 's/"/%22/g')
 
-echo $RIDGEPOLE_APPLY_LOG
+echo "$RIDGEPOLE_APPLY_LOG"
 
 echo "RIDGEPOLE_APPLY_LOG=$RIDGEPOLE_APPLY_LOG" >> $GITHUB_OUTPUT
 
