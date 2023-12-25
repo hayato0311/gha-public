42482b2f54f6cc448367e5b4bb795c6fb8724ce1
hayato0311 <hayato8088@gmail.com>
2023-12-25 18:12:37 +0900

    aaa

--- .github/workflows/bin/hoge.sh
+++ .github/workflows/bin/hoge.sh
@@ -2,7 +2,8 @@
 
 tmpfile=$(mktemp)
 
-echo "Apply `Schemafile`" >> $tmpfile
+echo "Starting ridgepole apply..." >> $tmpfile
+echo "Apply /`Schemafile/`" >> $tmpfile
 echo "-- create_table("internal_management_user_sessions", {:charset=>"utf8mb4", :collation=>"utf8mb4_general_ci"})" >> $tmpfile
 echo "-> 0.2156s" >> $tmpfile
 echo "-- add_index("internal_management_user_sessions", ["internal_management_user_id"], {:unique=>false, :name=>"idx_internal_management_user_sessions_1"})" >> $tmpfile
