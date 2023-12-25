07fff6ee889d11ebfc8d3e60faf2c53017daccb8
hayato0311 <hayato8088@gmail.com>
2023-12-25 18:06:14 +0900

    aaa

--- .github/workflows/bin/hoge.sh
+++ .github/workflows/bin/hoge.sh
@@ -2,10 +2,11 @@
 
 tmpfile=$(mktemp)
 
-echo "hoge  " >> $tmpfile
-echo "	hoge" >> $tmpfile
-echo "hoge" >> $tmpfile
-echo "hoge" >> $tmpfile
+echo "Apply `Schemafile`" >> $tmpfile
+echo "-- create_table("internal_management_user_sessions", {:charset=>"utf8mb4", :collation=>"utf8mb4_general_ci"})" >> $tmpfile
+echo "-> 0.2156s" >> $tmpfile
+echo "-- add_index("internal_management_user_sessions", ["internal_management_user_id"], {:unique=>false, :name=>"idx_internal_management_user_sessions_1"})" >> $tmpfile
+echo "-> 0.1132s" >> $tmpfile
 
 HOGE_LOG=$(cat $tmpfile)
 
